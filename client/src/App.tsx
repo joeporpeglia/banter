65b47cecd9465147f8c614b01f1318a5348d9484
Joe Porpeglia <josephp@spotify.com>
2020-04-19 12:17:35 -0400

    Login flow

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -15,6 +15,11 @@ import {
   Stack,
   Textarea,
   ThemeProvider,
+  Input,
+  FormControl,
+  FormLabel,
+  Switch,
+  Flex,
 } from '@chakra-ui/core';
 import React, {
   ChangeEvent,
@@ -24,28 +29,106 @@ import React, {
   useMemo,
   useReducer,
   useState,
+  createContext,
 } from 'react';
-import {
-  BrowserRouter,
-  Link as RouterLink,
-  Route,
-  RouteComponentProps,
-} from 'react-router-dom';
+import { BrowserRouter, Link as RouterLink, Route } from 'react-router-dom';
 import io from 'socket.io-client';
 
+const PlayerContext = createContext<Player | null>(null);
+
 function App() {
+  const [player, setPlayer] = useState<Player | null>(null);
+
   return (
-    <ThemeProvider>
-      <CSSReset />
-      <BrowserRouter>
-        <Suspense fallback={<Spinner />}>
-          <Route path="/" exact>
-            <HomePage />
-          </Route>
-          <Route path="/game/:gameId" exact component={GamePage} />
-        </Suspense>
-      </BrowserRouter>
-    </ThemeProvider>
+    <PlayerContext.Provider value={player}>
+      <ThemeProvider>
+        <CSSReset />
+        <BrowserRouter>
+          <Suspense fallback={<Spinner />}>
+            {player ? (
+              <LoggedIn player={player} />
+            ) : (
+              <LoggedOut setPlayer={setPlayer} />
+            )}
+          </Suspense>
+        </BrowserRouter>
+      </ThemeProvider>
+    </PlayerContext.Provider>
+  );
+}
+
+type LoggedInProps = {
+  player: Player;
+};
+
+function LoggedIn(props: LoggedInProps) {
+  const { player } = props;
+
+  return (
+    <>
+      <Route path="/" exact>
+        <HomePage />
+      </Route>
+      <Route
+        path="/game/:gameId"
+        exact
+        render={(route) => (
+          <GamePage gameId={route.match.params.gameId} player={player} />
+        )}
+      />
+    </>
+  );
+}
+
+type LoggedOutProps = {
+  setPlayer: (p: Player) => void;
+};
+
+function LoggedOut(props: LoggedOutProps) {
+  const { setPlayer } = props;
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    const savedPlayer: string | null = localStorage.getItem(
+      '@banter/saved-player',
+    );
+    if (savedPlayer) {
+      setPlayer(JSON.parse(savedPlayer));
+    }
+  }, [setPlayer]);
+
+  function handleSave() {
+    if (!name.trim().length) {
+      return;
+    }
+    const player: Player = {
+      playerName: name,
+    };
+
+    localStorage.setItem('@banter/saved-player', JSON.stringify(player));
+    setPlayer({
+      playerName: name,
+    });
+    setPlayer(player);
+  }
+
+  return (
+    <PageWrapper>
+      <Heading size="xl">Login</Heading>
+      <FormControl>
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input
+          id="username"
+          value={name}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setName(e.target.value)
+          }
+        />
+      </FormControl>
+      <Button size="lg" variant="solid" onClick={handleSave}>
+        Login
+      </Button>
+    </PageWrapper>
   );
 }
 
@@ -81,72 +164,62 @@ function generateGameId() {
   return gameId;
 }
 
-function GamePage(props: RouteComponentProps<{ gameId: string }>) {
-  const { gameId } = props.match.params;
-  const player: Player = useMemo(
-    () => ({
-      playerName: String(Math.random() * 10000),
-    }),
-    [],
-  );
+type GamePageProps = {
+  gameId: string;
+  player: Player;
+};
 
+function GamePage(props: GamePageProps) {
+  const { player, gameId } = props;
   const [game, dispatch] = useGame(gameId, player);
   const [promptText, setPromptText] = useState('');
   const isReady = game.playerReadyStatus[player.playerName] ?? false;
 
   return (
     <PageWrapper>
-      <pre>
-        <code>{JSON.stringify(game, null, 2)}</code>
-      </pre>
-      <Stack>
-        <Textarea
-          value={promptText}
-          onChange={(e: ChangeEvent<HTMLInputElement>) =>
-            setPromptText(e.target.value)
-          }
-        />
-        <Button
-          onClick={() => {
-            dispatch({
-              type: GameActions.AddPrompt,
-              prompt: {
-                promptId: String(Math.random()),
-                text: promptText,
-              },
-            });
-            setPromptText('');
-          }}
-        >
-          Add prompt
-        </Button>
-        {isReady ? (
+      <Stack spacing={6}>
+        <Box as="pre">
+          <code>{JSON.stringify(game, null, 2)}</code>
+        </Box>
+        <Stack spacing={2}>
+          <Textarea
+            value={promptText}
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setPromptText(e.target.value)
+            }
+          />
           <Button
-            variant="outline"
-            variantColor="red"
-            onClick={() =>
+            onClick={() => {
+              dispatch({
+                type: GameActions.AddPrompt,
+                prompt: {
+                  promptId: String(Math.random()),
+                  text: promptText,
+                },
+              });
+              setPromptText('');
+            }}
+          >
+            Add prompt
+          </Button>
+        </Stack>
+        <Flex justify="flex-end">
+          <FormLabel htmlFor="player-ready" fontSize="lg">
+            Ready?
+          </FormLabel>
+          <Switch
+            id="player-ready"
+            size="lg"
+            color="green"
+            isChecked={isReady}
+            onChange={() =>
               dispatch({
                 type: GameActions.SetReady,
-                isReady: false,
+                isReady: !isReady,
               })
             }
-          >
-            Unready
-          </Button>
-        ) : (
-          <Button
-            variant="outline"
-            variantColor="green"
-            onClick={() =>
-              dispatch({
-                type: GameActions.SetReady,
-                isReady: true,
-              })
-            }
-          >
-            Ready
-          </Button>
-        )}
+          />
+        </Flex>
       </Stack>
     </PageWrapper>
   );
